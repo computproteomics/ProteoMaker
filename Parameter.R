eb4929040b8e4eac64cb20f03a5208d5e1dab750
Veit Schwämmle <veitveit@gmail.com>
2020-01-15 09:15:13 +0100

    Update Parameter.R

--- Parameter.R
+++ Parameter.R
@@ -2,8 +2,17 @@
 ExpDesign <- rep(1:2,3)
 Param <- list(NumProteoforms=0)
 GroundTruth <- data.table()
-
 Digested <- data.table()
+
+# Parameter setting
+# Ground truth
+
+
+# Sample preparation
+
+
+# MS run
+
 
 
 
