626ab981144c95dfc15e662d760e21a03e34c199
Veit Schwämmle <veitveit@gmail.com>
2020-01-15 09:28:10 +0100

    Update Parameter.R

--- Parameter.R
+++ Parameter.R
@@ -1,17 +1,30 @@
 # Variables
 ExpDesign <- rep(1:2,3)
-Param <- list(NumProteoforms=0)
+Param <- list()
 GroundTruth <- data.table()
 Digested <- data.table()
 
-# Parameter setting
-# Ground truth
+### Parameter setting
+## Ground truth
+# Fraction of proteins getting PTMs
+Param$FracModProt <- 1
+# Fraction of modifiable proteins that get a PTM
+Param$FracModPerProt <- 2
+# PTM types
+Param$PTMTypes <- c("ph")
+# residues for PTM type
+Param$ModifiableResidues <- list()
+for (mod in Param$PTMTypes) 
+  Param$ModifiableResidues$mod <- c("S","T","Y")
+
+Param$NumProteoforms <- 0
+
+#
+
+## Sample preparation
 
 
-# Sample preparation
-
-
-# MS run
+## MS run
 
 
 
