e7fb1f128c41f0fb35468e27dd4d2541ae295884
Ludwig Lautenbacher <Ludwig.Lautenbacher@tum.de>
2020-01-16 16:41:02 +0100

    commented out the creation of the ground truth

--- Parameter.R
+++ Parameter.R
@@ -60,8 +60,9 @@ Param$ThreshNAProteoform <- -2
 #Use just to create proteoforms (modified and unmodified)
 # proteoforms <- samplePreparation(fasta.path = "fasta.fasta", parameters = Param)
 #Use to create the full structure of proteoforms along with abundances and ground truth expression patterns.
-#For example file (50 proteins)
-GroundTruth = addProteoformAbundance(proteoforms = samplePreparation(fasta.path = "fasta_example.fasta", parameters = Param), parameters = Param)
+
+# #For example file (50 proteins)
+# GroundTruth = addProteoformAbundance(proteoforms = samplePreparation(fasta.path = "fasta_example.fasta", parameters = Param), parameters = Param)
 #For whole human proteome
 GroundTruth = addProteoformAbundance(proteoforms = samplePreparation(fasta.path = "fasta_full_human.fasta", parameters = Param), parameters = Param)
 
