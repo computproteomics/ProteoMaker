1f0eb334f1c013fad83c74a77803ce592dc0d439
Eva Brombacher <eb162@pluto.uni-freiburg.de>
2020-01-16 08:36:41 +0100

    cleaned final data frame, updated expDataFrame.RData

--- formatExpDataTable.R
+++ formatExpDataTable.R
@@ -25,7 +25,6 @@ getCleanTable <- function(filepath){
 
         # Read file
         mtx <- fread(filepath_evidence, select = c("Sequence", "Leading razor protein", "Modified sequence", "Experiment", "Intensity", "Modifications"))
-
         pepStart <- fread(filepath_peptides, select = c("Sequence", "Start position"))
         mtx <- merge(mtx, pepStart, by="Sequence")
         
@@ -46,12 +45,10 @@ getCleanTable <- function(filepath){
         
         # Summarize rows with same modification and same experiment id by taking average of intensities
         mtx.aggr <- aggregate(Intensity ~ Sequence + `Start position` + `Leading razor protein` +`Modified sequence`+ Experiment  + prep, data=mtx, sum, na.rm=TRUE)
-
 }
 
+# combine the data frames corresonding to the three datasets
 formattedDF <- do.call(rbind,lapply(filepaths, getCleanTable))
-#write.csv(formattedDF, file=)
-#save(formattedDF, file="formattedDF.RData")
 
 # order prep increasing (phTyr before pThrSer)
 formattedDF <- formattedDF[order(formattedDF$prep),]
@@ -63,11 +60,22 @@ exPlan <- read.csv(exPlanPath, allowEscapes = TRUE, check.names = FALSE,sep = "\
 mapping <- data.frame(key=exPlan$label,value=exPlan$condition)
 replicate <- gsub(".*(\\d+)", "\\1", formattedDF$Experiment)
 mapping <- setNames(exPlan$condition, exPlan$label)[formattedDF$Experiment]
-
 formattedDF$expGroup <- paste(mapping, replicate, sep="_")
+
+# keep only 4 replicates
+formattedDF <- formattedDF[!(as.numeric(str_sub(formattedDF$expGroup, start= -1)) > 4),]
+
 formattedDF <- formattedDF[grepl("control|EGF15|mitosis",formattedDF$expGroup),]
 
+# control -> C_1_R_; EGF15 -> C_2_R_; mitosis -> C_3_R_
+formattedDF$expGroup <- gsub("control", "C_1_R", formattedDF$expGroup)
+formattedDF$expGroup <- gsub("EGF15", "C_2_R", formattedDF$expGroup)
+formattedDF$expGroup <- gsub("mitosis", "C_3_R", formattedDF$expGroup)
+
+
+
 library(reshape2)
+formattedDF$Intensity <- as.numeric(formattedDF$Intensity)
 formattedDFcast <- dcast(formattedDF, Sequence + `Start position` + `Leading razor protein` + `Modified sequence` ~ expGroup, value.var = c("Intensity"))
 
 library(stringr)
@@ -96,4 +104,13 @@ formattedDFcast$PTMpos <- lapply(seq_along(PTMinfo$PTM_pos), function(x) {
     PTMinfo$PTM_pos[[x]] + formattedDFcast$`Start position`[x] - 1
 })
 
-# save(formattedDFcast, file="expDataFrame.RData")
+formattedDFcast$`Modified sequence` <- gsub("_","", formattedDFcast$`Modified sequence`)
+
+library(dplyr)
+finalDF <- formattedDFcast %>%
+    select("Sequence", "Leading razor protein", "PTMpos", "PTMtype", everything())
+finalDF$`Modified sequence` <- NULL
+
+names(finalDF)[names(finalDF) == 'Leading razor protein'] <- 'Accession'
+
+# save(finalDF, file="expDataFrame.RData")
