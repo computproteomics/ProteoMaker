e35535c0de73b9ebe81a9d0d70108b285bdbb37f
Marie Locard-Paulet <marie.locard-paulet@cpr.ku.dk>
2020-01-16 14:57:45 +0100

    digestion - with warnings

--- Digestion.R
+++ Digestion.R
@@ -2,25 +2,40 @@ source("Parameter.R")
 
 library(OrgMassSpecR)
 
+# proteoformsRow <- proteoforms[58,]
+
 getDigestTables <- function(proteoformsRow) {
-    df <- Digest(proteoformsRow$Sequence,enzyme = "trypsin", missed = 0)
-    df$Accession <- proteoformsRow$Accession
-    df$PTMPos <- proteoformsRow$PTMPos
-    df$PTMType <- proteoformsRow$PTMType
-    df
+  df <- Digest(proteoformsRow$Sequence,enzyme = "trypsin", missed = 0)
+  df$Accession <- proteoformsRow$Accession
+  df$PTMPos <- NA
+  df$PTMType <- NA
+  if (!is.null(proteoformsRow$PTMPos)) {
+    for (i in seq_len(nrow(df))) {
+      sel <- unlist(proteoformsRow$PTMPos) >= df$start[i] & unlist(proteoformsRow$PTMPos) <= df$stop[i]
+      if (any(sel)) {
+        df$PTMType[i] <- c(unlist(proteoformsRow$PTMType)[sel])
+        df$PTMPos[i] <- c(unlist(proteoformsRow$PTMPos)[sel])
+      } else {
+        df$PTMPos[i] <- NA
+        df$PTMType[i] <- NA
+      }
+    }
+  }
+  df
 }
 
-combined <- do.call(rbind,apply(proteoforms, 1, function(x) getDigestTables(x)))
+ldig <- lapply(seq_len(nrow(proteoforms)), function(x) {
+  getDigestTables(proteoforms[x,])
+})
+peptable <- ldig[[1]]
+for (i in 2:length(ldig)) {
+  peptable <- rbind(peptable, ldig[[i]])
+}
 
-# PTMs <- data.frame(key=proteoforms$PTMPos[36], value=proteoforms$PTMType[36])
-# names(PTMs) <- c("position", "modification")
-# 
-# # if ptm in range of start to stop for fragment add ptm
-# fragments$start
-# fragments$stop
+names(peptable)[names(peptable) == "peptide"] <- "PepSequence"
+names(peptable)[names(peptable) == "start"] <- "PepStart"
+names(peptable)[names(peptable) == "stop"] <- "PepStop"
 
-# row wise comparison with PTMs table
-#fragments[(PTMs$position[1]>=fragments$start) & (PTMs$position[1]<=fragments$stop), ]$PTMType <- toString(PTMs$modification[1])
+insilico_peptides <- peptable
 
-#Param$PepMinLength
-#Param$PepMaxLength
+save(insilico_peptides, file = "data/insilicoPep.RData")
