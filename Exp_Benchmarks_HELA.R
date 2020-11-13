a3e1f472ed96b011bc38569a5ac4a0aa6da77541
veitveit <veitveit@gmail.com>
2020-11-13 13:35:36 +0100

    now 2 different scripts for exp. benchmarks

---
+++ Exp_Benchmarks_HELA.R
@@ -0,0 +1,129 @@
+library(moments)
+library(stringr)
+source ("06_Benchmarks.R")
+
+readMaxQuant <- function(allPeps, Prots, Param=NULL) {
+  allPeps$Accession <- sapply(allPeps$Proteins, function(x) strsplit(as.character(x), ";"))
+  allPeps$MC <- allPeps$Missed.cleavages
+  allPeps$PTMType <- as.character(allPeps$Modifications)
+  allPeps$PTMType[allPeps$PTMType == "Unmodified"] <- "NULL"
+  # did not find corresponding field    
+  allPeps$PTMPos <- NA
+  allPeps$PTMPos[allPeps$PTMType != "NULL"] <- 1
+  
+  # remove entries with missing protein name (should be reverse)
+  allPeps <- allPeps[allPeps$Proteins != "",]
+  
+  Param$QuantColnames <- grep("Intensity\\.", names(allPeps), value=T)
+  
+  # TODO: check whether LFQ results are available 
+  #protCols <- grepl("LFQ.intensity", names(Prots))
+  #names(Prots)[protCols] <- Param$QuantColnames
+  Prots$Accession <- Prots$Sequence <- Prots$Protein.IDs
+  Prots$num_accs <- Prots$Proteins
+  
+  # filter for rows with no quantifications
+  tquant <- allPeps[,Param$QuantColnames]
+  tquant[tquant == 0] <- NA
+  allPeps[, Param$QuantColnames] <- log2(tquant)
+  allPeps <- allPeps[rowSums(is.na(allPeps[, Param$QuantColnames,drop=F])) < length(Param$QuantColnames), ]
+  tquant <- Prots[,Param$QuantColnames]
+  allPeps$Sequence <- as.character(allPeps$Sequence)
+  tquant[tquant == 0] <- NA
+  Prots[, Param$QuantColnames] <- log2(tquant)
+  Prots <- Prots[rowSums(is.na(Prots[, Param$QuantColnames,drop=F])) < length(Param$QuantColnames), ]
+  rownames(Prots) <- Prots$Accession
+  # add column with miscleavages
+  Prots$MC <- NA
+  if (!is.null(allPeps$MC)) {
+    mergedMCs <- unlist(by(allPeps$Missed.cleavages, as.character(allPeps$Proteins), function(x) paste(x,collapse=";")))
+    Prots[names(mergedMCs), "MC"] <- mergedMCs
+  } else {
+    allPeps$MC <- as.character(0)
+    Prots$MC <- as.character(0)
+  }
+  
+  return(list(Param=Param, allPeps=allPeps, Prots=Prots))
+}
+
+
+############ Call the function to load and prepare data, and then to calculate the benchmarks
+### It is now adopted to a set of zip files in a given folder
+library(jsonlite)
+## General parameters
+withGroundTruth <- F
+tmpdir <- "tmp"
+tmpdir <- normalizePath(tmpdir)
+
+allzips <- grep(".zip",list.files(),value=T)
+
+for (zip in allzips)  {
+  
+  filelist <- system(paste0("jar tf ", zip), intern=T)
+  # non-optimal: takes only the first of the files if multiples are available in zip-file     
+  if (any(grepl("modificationSpecificPeptides.txt",filelist)) & any(grepl("proteinGroups.txt",filelist))) {
+    #	    print(filelist)
+    dfile <- grep("modificationSpecificPeptides.txt",filelist, value=T)[1]
+    system(paste0("cd ",tmpdir,";jar xvf ",tempfile," \"",dfile,"\""))
+    tdfile <- paste0(tmpdir,"/", dfile)
+    modpep <- read.csv(tdfile, sep="\t")
+    file.remove(tdfile)
+    dfile <- grep("proteinGroups.txt",filelist, value=T)[1]
+    tdfile <- paste0(tmpdir,"/", dfile)
+    system(paste0("cd ",tmpdir,";jar xvf ",tempfile," \"",dfile,"\""))
+    protlist <- read.csv(tdfile, sep="\t")
+    file.remove(tdfile)
+  }
+  file.remove(tempfile)
+  
+  # if both files are non-zero
+  if (!is.null(modpep) & !is.null(protlist)) {
+    try({
+      MQout <- readMaxQuant(modpep, protlist)
+      allPeps <- MQout$allPeps
+      Prots <- MQout$Prots
+      Param <- MQout$Param
+      Benchmarks <- NULL
+      rownames(allPeps) <- paste0("pep", 1:nrow(allPeps))
+      if (withGroundTruth) {
+        Stats <- runPolySTest(Prots, Param, refCond=1, onlyLIMMA=F)
+        # much faster with only LIMMA tests   
+        StatsPep <- runPolySTest(allPeps, Param, refCond=1, onlyLIMMA=T)
+        Benchmarks <- calcBenchmarks(Stats, StatsPep, Param)
+      } else {
+        Benchmarks <- calcBasicBenchmarks(Prots, allPeps, Param)
+      }
+      
+      ### Get additional benchmarks only for experimental data
+      ## Max. difference retention time
+      Benchmarks$globalBMs$diffRetentionTime <- diff(range(MQout$allPeps$Retention.time))
+      
+      ## Number of accepted PSMs (count scan numbers)
+      Benchmarks$globalBMs$acceptedPSMs <- sum(MQout$allPeps$MS.MS.Count)
+      
+      save(Benchmarks, Prots, allPeps, Param, tdat, file =paste0("benchmarks_",zip, ".RData") )
+    })
+  }
+}
+
+
+
+benchmarks <- list.files("./","benchmarks.*RData")
+AllExpBenchmarks <- NULL
+for (bench in benchmarks) {
+  load(bench)
+  print(bench)
+  if (withGroundTruth) {
+    Stats <- runPolySTest(Prots, Param, refCond=1, onlyLIMMA=F)
+    # much faster with only LIMMA tests   
+    StatsPep <- runPolySTest(allPeps, Param, refCond=1, onlyLIMMA=T)
+    Benchmarks <- calcBenchmarks(Stats, StatsPep, Param)
+  } else {
+    Benchmarks <- calcBasicBenchmarks(Prots, allPeps, Param)
+  }
+  save(Benchmarks, Prots, allPeps, Param, tdat, file =bench)
+  AllExpBenchmarks <- rbind(c(unlist(Benchmarks$globalBMs), tdat))
+}
+rownames(AllExpBenchmarks) <- benchmarks
+save(AllExpBenchmarks, file="AllExpBenchmarks.RData")
+
